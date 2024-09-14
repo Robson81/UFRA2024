3a5136a999e70d3e26be297e8e813ccb9303d441
Robson81 <63528191+Robson81@users.noreply.github.com>
2024-09-14 18:30:54 -0300

    Update MainConta.java

--- MainConta.java
+++ MainConta.java
@@ -5,16 +5,14 @@ public class MainConta {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-	ContaBancaria c1 = new ContaEspecial("Banco UFRA", 0001, 1001, 0.0, "Aluno BSI UFRA 01", 200.0);
-	
-	ContaBancaria c2 = new ContaBancaria("Banco UFRA", 0001, 1002, 0.0, "Aluno BSI UFRA 02");
-	
-	//ContaBancaria c3 = new ContaEspecial(null, 0, 0, 0, null, 0)
-	
-	
-	System.out.println("Dados Bancários do 1º Cliente: \n\n" + c1);
-	System.out.println("\n******************************\n");
-	System.out.println("Dados Bancários do 2º Cliente: \n\n" + c2);
+	        ContaBancaria c1 = new ContaBancaria("Banco UFRA", 0001, 1002, 0.0, "Aluno BSI UFRA 02");
+		System.out.println();
+		ContaBancaria c2 = new ContaEspecial("Banco UFRA", 0001, 1001, 0.0, "Aluno BSI UFRA 01", 200.0);
+		System.out.println();		
+		
+		System.out.println("Dados Bancários do 1º Cliente: \n\n" + c1);
+		System.out.println("\n******************************\n");
+		System.out.println("Dados Bancários do 2º Cliente: \n\n" + c2);
 
 	
 	
